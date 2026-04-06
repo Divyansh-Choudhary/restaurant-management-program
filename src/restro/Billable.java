5e843e72e321f2638244fd1450649bb78673bba2
D <73731187+Divyansh-Choudhary@users.noreply.github.com>
2026-04-06 10:01:27 +0530

    Create Billable interface for billing operations
    Added Billable interface for billing operations.

---
+++ src/restro/Billable.java
@@ -0,0 +1,22 @@
+/**
+ * Interface for billing operations
+ * Includes methods for calculating total cost and printing receipts.
+ */
+
+package restro;
+
+interface Billable {
+
+  /**
+   * Calculates the total of the given menu/order.
+   *
+   * @params menu - the menu of order containing the items
+   * @return the total amount of all items
+   */
+  double calculateTotal(Menu menu);
+
+  /*
+   * Prints the receipt of the menu
+   */
+  void printReceipt();
+}
