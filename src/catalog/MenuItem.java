b72f98c1d311ddbbf433337aa18e4997178f7487
kanchan6126 <kanchan2006g@gmail.com>
2026-04-06 23:34:19 +0530

    Add MenuItem class for catalog management

---
+++ src/catalog/MenuItem.java
@@ -0,0 +1,19 @@
+package catalog
+import java.util.Scanner;
+
+class MenuItem {
+    public int id;
+    public String name;
+    public double price;
+
+    public MenuItem(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public void displayItem() {
+        System.out.println("ID: " + id + ", Name: " + name + ", Price: " + price);
+    }
+}
+
