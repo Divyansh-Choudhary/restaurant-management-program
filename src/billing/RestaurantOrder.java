0958bcc9ec4010e7156c8a876e01def9a36e6e7b
D <73731187+Divyansh-Choudhary@users.noreply.github.com>
2026-04-06 23:06:30 +0530

    Refactor RestaurantOrder class for better access and clarity

--- src/billing/RestaurantOrder.java
+++ src/billing/RestaurantOrder.java
@@ -5,75 +5,84 @@ import people.*;
 
 public class RestaurantOrder implements Billable{
 
-    int orderId;
-    int customerId;
-    int[][] orderedItems;
-    int itemCount;
-    double finalTotal;
-    
-      RestaurantOrder(int orderId , int customerId ){
-          this.orderId = orderId;
-          this.customerId = customerId;
-          this.orderedItems = new int[50][3];
-          this.itemCount = 0;
-          this.finalTotal = 0;
-      }
-      void addItemToOrder(int id , int qnt){
-          for(int i=0; i<orderedItems.length; i++){
-              if(orderedItems[i][0] == id ){
-                  orderedItems[i][1] += qnt;
-                  return;
+    public int orderId;
+    public int customerId;
+    public int[][] orderedItems;
+    public int itemCount;
+    public double finalTotal;
+    
+    public RestaurantOrder(int orderId , int customerId ){
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderedItems = new int[50][3];
+        this.itemCount = 0;
+        this.finalTotal = 0;
+    }
+    
+    public void addItemToOrder(int id , int qnt){
+        for(int i = 0; i < orderedItems.length; i++){
+            if(orderedItems[i][0] == id ){
+                orderedItems[i][1] += qnt;
+                return;
+            
+            }
+        
+        }
+    
+        orderedItems[itemCount][0] = id;
+        orderedItems[itemCount][1] = qnt;
+        itemCount++;
+    }
+    
+    public double calculateTotal(Menu menu){
+        this.finalTotal = 0;
 
-              }
+        if(itemCount == 0) return 0.00;
+        
+        for(int i = 0; i < itemCount; i++){
+            int itemId = orderedItems[i][0];
+            int itemQnt = orderedItems[i][1];  
+            for (int j = 0; j < menu.items.length; j++){
+                MenuItem item = menu.items[j];
+                if(item.id == itemId){
+                    this.finalTotal += item.price*itemQnt;
+                    break;
+                }
+            }
+        }
+    
+        return finalTotal;
+    }
+    
+    public void printReceipt(Menu menu, Customer customer){
+        int menuLength = menu.itemCount;
+        
+        String receipt = "Bill no : " + orderId + "\n";
 
-          }
+        receipt += "\nCustomer name: " + customer.name;
+        receipt += "\nMobile number: " + customer.mobileNumber;
+                
+        receipt += "=======================";
+        receipt += ">>   Name\tPrice    Qnt    Cost\t";
 
-          ordredItems[itemCount++] = {id,qnt};
-      }
+        if(itemCount == 0){
+            receipt += "Nothing\tto\teat.\nNothing\tto\tdrink.\n";
+            receipt += "\nWe Hope to see you spending some penny next time.";
+        }else{
+            for(int i = 0; i < itemCount; i++){
+                int itemId = orderedItems[i][0];
+                int itemQnt = orderedItems[i][1];  
+                for (int j = 0; j < menuLength; j++){
+                    MenuItem item = menu.items[j];
+                    if(item.id == itemId){
+                        receipt+= i + ". " + item.name + "\t" + item.price + "    " + itemQnt + "    " + (double)(item.price * itemQnt);
+                    }
+                }
+            }
+            receipt += "\tGrand Total:\t" + this.calculateTotal(menu);
 
-      void calculateTotal(Menu menu){
-          int menuLength = menu.itemCount;
-
-          for(int i=0; i<itemCount; i++){
-              int itemId = orderedItems[i][0];
-              int itemQnt = orderedItems[i][1];  
-              for (int j=0; j<menuLength; j++){
-                  MenuItem item = menu.items[j];
-                  if(item.id == itemId){
-                      finalTotal += item.price*itemQnt;
-                      break;
-                  }
-              }
-          }
-
-
-      }
-      
-      void printReceipt(Menu menu, Customer cust){
-          int menuLength = menu.itemCount;
-
-          String receipt = "Bill no : " + orderId + "\n";
-
-          for(int i = 0; i < cust.length; i++){
-              if(cust.id == customerId){
-                  receipt += "\nCustomer name: " + cust.name;
-                  receipt += "\nMobile number: " + cust.mobileNumber;
-              }
-          }
-
-          receipt += "=======================";
-          receipt += ">>   Name\tPrice    Qnt    Cost";
-          for(int i=0; i<itemCount; i++){
-              int itemId = orderedItems[i][0];
-              int itemQnt = orderedItems[i][1];  
-              for (int j=0; j<menuLength; j++){
-                  MenuItem item = menu.items[j];
-                  if(item.id == itemId){
-                      receipt+= i + ". " + item.name + "\t" + item.price "    " + itemQnt + "    " + (double)(item.price * itemQnt);
-                  }
-              }
-          }
-          receipt += "\tGrand Total:\t" + finalTotal;
-
-      }
+            receipt += "";
+        }
+        
+    }
 }
